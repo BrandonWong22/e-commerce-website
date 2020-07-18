1a09e0a460e89adf1d6c633dea3769377a7a7fac
brandonwong22 <brandonwong2608@gmail.com>
2020-07-18 14:42:17 -0400

    create form with custom input fields and buttons for user sign in

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import Shop from "./pages/Shop/Shop";
 import Header from "./components/Header/Header";
+import SignInSignOut from "./pages/SignInSignOut/SignInSignOut";
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={Shop} />
+          <Route path="/signin" component={SignInSignOut} />
         </Switch>
       </BrowserRouter>
     </>
---
+++ src/pages/SignInSignOut/SignInSignOut.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./SignInSignOut.scss";
+import SignIn from "../../components/SignIn/SignIn";
+
+const SignInSignOut = () => {
+  return (
+    <div className="sign-in-sign-out">
+      <SignIn></SignIn>
+    </div>
+  );
+};
+
+export default SignInSignOut;
