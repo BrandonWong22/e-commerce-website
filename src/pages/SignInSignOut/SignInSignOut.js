163a56cd95be0d22842fc9ae47aa701730396e90
brandonwong22 <brandonwong2608@gmail.com>
2020-07-20 11:51:14 -0400

    add firebase email and password sign up

--- src/pages/SignInSignOut/SignInSignOut.js
+++ src/pages/SignInSignOut/SignInSignOut.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./SignInSignOut.scss";
 import SignIn from "../../components/SignIn/SignIn";
+import SignUp from "../../components/SignUp/SignUp";
 
 const SignInSignOut = () => {
   return (
     <div className="sign-in-sign-out">
-      <SignIn></SignIn>
+      <SignIn />
+      <SignUp />
     </div>
   );
 };
