b048c500d65735eb98184aa860eafa07a7bea339
brandonwong22 <brandonwong2608@gmail.com>
2020-07-20 19:33:54 -0400

    add card utils to add to quantity property when item is pressed more than once

--- src/components/CollectionItem/CollectionItem.js
+++ src/components/CollectionItem/CollectionItem.js
@@ -1,8 +1,12 @@
 import React from "react";
 import CustomButton from "../CustomButton/CustomButton";
 import "./CollectionItem.scss";
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/CartActions";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+
   return (
     <div className="collection-item">
       <div
@@ -15,9 +19,15 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <CustomButton inverted>Add to cart</CustomButton>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
